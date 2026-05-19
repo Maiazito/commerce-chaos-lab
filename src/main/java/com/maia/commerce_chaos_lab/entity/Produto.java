19eb33a3be13e246ca59c69acb6d75f8e4d636a9
maiazito <marcus.maia@yahoo.com.br>
2026-05-19 10:33:18 -0300

    Implementa optimistic locking para controle concorrente de estoque

--- src/main/java/com/maia/commerce_chaos_lab/entity/Produto.java
+++ src/main/java/com/maia/commerce_chaos_lab/entity/Produto.java
@@ -1,9 +1,6 @@
 package com.maia.commerce_chaos_lab.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,5 +25,8 @@ public class Produto {
     private BigDecimal preco;
 
     private Integer estoque;
+
+    @Version
+    private Long version;
 
 }
--- src/main/java/com/maia/commerce_chaos_lab/service/CompraService.java
+++ src/main/java/com/maia/commerce_chaos_lab/service/CompraService.java
@@ -5,7 +5,9 @@ import com.maia.commerce_chaos_lab.entity.Produto;
 import com.maia.commerce_chaos_lab.repository.PedidoRepository;
 import com.maia.commerce_chaos_lab.repository.ProdutoRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
+
 
 import java.time.LocalDateTime;
 
@@ -15,44 +17,56 @@ public class CompraService {
     private final ProdutoRepository produtoRepository;
     private final PedidoRepository pedidoRepository;
 
-
-    public CompraService(ProdutoRepository produtoRepository,
-                         PedidoRepository pedidoRepository
-    ){
+    public CompraService(
+            ProdutoRepository produtoRepository,
+            PedidoRepository pedidoRepository
+    ) {
         this.produtoRepository = produtoRepository;
         this.pedidoRepository = pedidoRepository;
     }
 
     @Transactional
-    public String comprar(Long produtoId){
-        Produto produto  = produtoRepository.findById(produtoId)
-                .orElseThrow(()->
-                        new RuntimeException("Produto não encontrado")
-                );
+    public String comprar(Long produtoId) {
 
-        if (produto.getEstoque() <=0){
-            return "Produto sem toque";
-        }
-        //janela de concorrência
         try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+            Produto produto = produtoRepository.findById(produtoId)
+                    .orElseThrow(() ->
+                            new RuntimeException("Produto não encontrado")
+                    );
+
+            if (produto.getEstoque() <= 0) {
+                return "Produto sem estoque";
+            }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            produto.setEstoque(
+                    produto.getEstoque() - 1
+            );
+
+            produtoRepository.save(produto);
+
+            Pedido pedido = new Pedido();
+
+            pedido.setValorTotal(produto.getPreco());
+
+            pedido.setDataCriacao(LocalDateTime.now());
+
+            pedido.setStatus("REALIZADO");
+
+            pedidoRepository.save(pedido);
+
+            return "Compra realizada com sucesso";
+
+        } catch (ObjectOptimisticLockingFailureException e) {
+
+            return "Outro usuário comprou este produto ao mesmo tempo";
+
         }
-
-
-        produto.setEstoque(
-                produto.getEstoque()-1
-        );
-
-        produtoRepository.save(produto);
-
-        Pedido pedido = new Pedido();
-
-        pedido.setValorTotal (produto.getPreco());
-        pedido.setDataCriacao(LocalDateTime.now());
-        pedido.setStatus("Realizado");
-        pedidoRepository.save(pedido);
-        return "Compra realizado com sucesso";
     }
 }
